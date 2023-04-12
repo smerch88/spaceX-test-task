ce300752ae55dc8109481c1abd152f6c59d22cd4
smerch88 <maxarmyk@gmail.com>
2023-04-12 21:57:42 +0300

    feat: add navigation to links

---
+++ pages/favourites.tsx
@@ -0,0 +1,9 @@
+import { withLayout } from '@/layout/Layout';
+import { HeroSection } from '@/page-components/Hero';
+import { ToursSection } from '@/page-components/Tours';
+
+const Favourites = () => {
+  return <></>;
+};
+
+export default withLayout(Favourites);
